1923e66222ea03a57229ce1f68a33f0f8adda474
ashleymb989 <ashleymb.989@gmail.com>
2026-08-02 18:58:16 -0700

    Import motion from framer-motion in Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import nevaMealsImg from "@/assets/portfolio/neva-meals.jpg";
 import { ArrowUpRight, ArrowLeft } from "lucide-react";
 import { Link } from "react-router-dom";
